5cd7a20d3630eba91c23b153bffa8497aae7637d
Jaka Hudoklin <jakahudoklin@gmail.com>
2013-04-28 00:39:47 +0200

    add support for hostname based on mac, add miner.litecoin.si to hosts and first support for zabbix agent active checks

--- miner.nix
+++ miner.nix
@@ -11,8 +11,8 @@ with pkgs.lib;
       ./hardware.nix
     ];
 
-  networking.hostName = "worker.miner.litecoin.si";
   networking.nameservers = [ "8.8.8.8" "4.4.4.4" ];
+  networking.extraHosts = ''10.4.0.1 miner.litecoin.si'';
 
   # Select internationalisation properties.
   i18n = {
@@ -31,22 +31,24 @@ with pkgs.lib;
   services.xserver.videoDrivers = ["ati_unfree"];
   services.xserver.exportConfiguration = false;
 
-  system.activationScripts.drifix =
-    ''
+  system.activationScripts.drifix = ''
       # Create the required /usr/lib/dri/fglrx_dri.so;
       mkdir -p /usr/lib/dri
       ln -fs /run/opengl-driver/lib/fglrx_dri.so /usr/lib/dri/fglrx_dri.so
     '';
 
-  system.activationScripts.xorg =
-    ''
+  system.activationScripts.xorg = ''
       LD_LIBRARY_PATH=/run/opengl-driver/lib:/run/opengl-driver-32/lib ${pkgs.linuxPackages.ati_drivers_x11}/bin/aticonfig --initial --adapter=all --output=/etc/X11/xorg.conf
     '';
 
-  system.activationScripts.openvpn =
-    ''
+  system.activationScripts.openvpn = ''
       MAC=$(${pkgs.nettools}/bin/ifconfig | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | tr -d ":")
       echo -e "$MAC.$(dnsdomainname)\npass" > /var/run/openvpn-pass.txt
+    '';
+
+  system.activationScripts.hostname = ''
+      MAC=$(${pkgs.nettools}/bin/ifconfig | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | tr -d ":")
+      hostname "$MAC.miner.litecoin.si"
     '';
 
   security.sudo.enable = true;
@@ -95,6 +97,8 @@ with pkgs.lib;
     server = "10.4.0.1";
     extraConfig = "
       EnableRemoteCommands=1
+      Hostname=miner.litecoin.si
+      DisableActive=0
     ";
   };
 
